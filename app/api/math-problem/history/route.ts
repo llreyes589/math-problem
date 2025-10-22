e69bca9ff93fc3065ba25e7a9d6460745a4eb784
Lester Lou Reyes <llreyes.589@gmail.com>
2025-10-21 21:17:24 -0600

    make route.ts dynamic

--- app/api/math-problem/history/route.ts
+++ app/api/math-problem/history/route.ts
@@ -1,7 +1,12 @@
 import { NextResponse } from "next/server";
 import { supabase } from "../../../../lib/supabaseClient";
 
-export async function GET() {
+export async function GET(request) {
+  // Add a condition that cannot be resolved statically by Next.js
+  if (request.url.length < 0) {
+    // This condition is always false, but makes the route dynamic
+    return new Response("Error");
+  }
   let { data: math_problem_submissions, error } = await supabase
     .from("math_problem_submissions")
     .select(
